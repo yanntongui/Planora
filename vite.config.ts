a24bd9a12e7494615ae8b6f046a5b89f5e3678ab
yanntongui <yannpatrick.tongui@gmail.com>
2026-02-01 01:42:15 +0100

    Chore: Adjust chunkSizeWarningLimit in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,9 @@ export default defineConfig(({ mode }) => {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
+    build: {
+      chunkSizeWarningLimit: 1000,
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
